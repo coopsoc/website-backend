085f083545f5419554f7c105062a4bf0961173dd
timesth <timesth16@gmail.com>
2024-05-15 19:40:59 +1000

    install ts-node and start code

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 
+const stripe = require("stripe")(
+  "sk_test_51PGdg7HTy0Cdu2FbkJXV5jx5ghPbjmLpPPvVVd1CSR5IbyovazKdB7BwGPiwJu1GPXTWyrEuIYbgTroYdWbJ7Sb900GztJ4miW"
+);
+async function getProducts() {
+    const products = await stripe.products.list().then(products=>{return products});
+}
+
+console.log(getProducts());
